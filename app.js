45a0a308c6037b0c5eb00e4738451eec5a164637
Louie Adamian <louieadamian@gmail.com>
2018-07-15 16:48:50 -0400

    finishes front page

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 "use strict";
-var port = 3000;
+
 const pug = require('pug');
 const express = require('express');
 const app = express();
+
+const port = 3000;
+
 app.set('view engine','pug');
 app.use(express.static('www'));
 app.set('views', __dirname + '/views');
 app.get('/', function(req, res){
   res.render("index");
+  res.render("mission");
 });
 
 app.listen(port, () => console.log('Tesselating gardens listening on ' + port));
-
-}
-
-app.listen(3000)
